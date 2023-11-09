7e5fb8eec8e1fc73da95446b1f287baaec23506f
Juan <Harringtonjuanc@gmail.com>
2023-11-08 20:39:43 -0500

    Initial Commit

---
+++ ConcurrencyImplement/ConcurrencyImplementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyImplementApp.swift
+//  ConcurrencyImplement
+//
+//  Created by Juan Harrington on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyImplementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
